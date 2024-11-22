369a4f4c7b446df905d044651cc016bbc0246d72
Florian Sieradzki <floriansieradzki@gmail.com>
2024-11-22 17:58:49 +0100

    Reduce indentation

--- exercise/c#/Day02/Day02/FizzBuzz.cs
+++ exercise/c#/Day02/Day02/FizzBuzz.cs
@@ -2,42 +2,48 @@
 {
     public static class FizzBuzz
     {
+        private const int Max = 100;
+        private const int Min = 0;
+        private const int Fizz = 3;
+        private const int Buzz = 5;
+
         public static string Convert(int input)
         {
-            if (input > 0)
-            {
-                if (input <= 100)
-                {
-                    if (IsFizz(input) && IsBuzz(input))
-                    {
-                        return "FizzBuzz";
-                    }
+            if (IsOutOfRange(input)) throw new OutOfRangeException();
 
-                    if (IsFizz(input))
-                    {
-                        return "Fizz";
-                    }
-
-                    if (IsBuzz(input))
-                    {
-                        return "Buzz";
-                    }
-
-                    return input.ToString();
-                }
-                else
-                {
-                    throw new OutOfRangeException();
-                }
-            }
-            else
-            {
-                throw new OutOfRangeException();
-            }
+            return ToFizzBuzz(input);
         }
 
-        private static bool IsFizz(int input) => input % 3 == 0;
+        private static bool IsOutOfRange(int input) => LessOrEqualThanMin(input) || GreaterThanMax(input);
 
-        private static bool IsBuzz(int input) => input % 5 == 0;
+        private static string ToFizzBuzz(int input)
+        {
+            if (IsFizz(input) && IsBuzz(input))
+            {
+                return "FizzBuzz";
+            }
+
+            if (IsFizz(input))
+            {
+                return "Fizz";
+            }
+
+            if (IsBuzz(input))
+            {
+                return "Buzz";
+            }
+
+            return input.ToString();
+        }
+
+        private static bool GreaterThanMax(int input) => input > Max;
+
+        private static bool LessOrEqualThanMin(int input) => input <= Min;
+
+        private static bool IsFizz(int input) => Is(input, Fizz);
+
+        private static bool IsBuzz(int input) => Is(input, Buzz);
+
+        private static bool Is(int input, int divisor) => input % divisor == 0;
     }
 }
